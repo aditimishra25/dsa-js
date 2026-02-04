ebb125af157b8c72680b1a67d12aaebdceaa7857
Aditi Mishra <aditimishra2501@gmail.com>
2026-02-04 14:06:58 +0530

    valid palindrome and remove duplicates

---
+++ two-pointers/valid-palindrome.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let left = 0, right = s.length - 1
+
+    while (left < right) {
+        while (!/[a-zA-Z0-9]/.test(s[left])) left++;
+        while (!/[a-zA-Z0-9]/.test(s[right])) right--;
+
+        if (left< right && s[left].toLowerCase() != s[right].toLowerCase()) return false;
+
+        left++, right--
+    }
+    return true;
+};
