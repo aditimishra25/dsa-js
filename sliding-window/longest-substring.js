8dd0a348d25f931ffedd4c19503792354dfa211d
Aditi Mishra <aditimishra2501@gmail.com>
2026-02-06 15:56:47 +0530

    longest substring

---
+++ sliding-window/longest-substring.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+/* used dynamic sliding window as length of longest substring is not known 
+
+intuition: 
+        A Set tracks the unique characters within the current window. 
+        If a duplicate is encountered, the left side of the window is contracted    
+        until the substring becomes unique again, ensuring only unique characters 
+        are ever within the window. 
+*/
+var lengthOfLongestSubstring = function (s) {
+    let left = 0
+    let maxLength = 0;
+    let unique = new Set();
+
+    for (let right = 0; right < s.length; right++) {
+        while (unique.has(s.charAt(right))) {
+            unique.delete(s.charAt(left))
+            left++;
+        }
+        unique.add(s.charAt(right))
+        maxLength = Math.max(maxLength, right - left + 1)
+    }
+
+    return maxLength;
+}
