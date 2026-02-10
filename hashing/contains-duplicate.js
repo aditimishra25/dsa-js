df4a4f591a877ede7a810c3d593b06993fc039cb
Aditi Mishra <aditimishra2501@gmail.com>
2026-02-10 14:22:36 +0530

    contains duplicate

---
+++ hashing/contains-duplicate.js
@@ -0,0 +1,25 @@
+/**
+ * Checks whether the array contains any duplicate elements.
+ *
+ * @param {number[]} nums - Input array of numbers
+ * @return {boolean} - Returns true if any number appears more than once, otherwise false
+ */
+var containsDuplicate = function (nums) {
+    // Create a Set to store numbers we have already seen
+    let set = new Set();
+
+    // Iterate through each number in the array
+    for (let num of nums) {
+        // If the number already exists in the set,
+        // it means we found a duplicate
+        if (set.has(num)) {
+            return true;
+        } 
+        
+        // Otherwise, add the number to the set
+        set.add(num);
+    }
+
+    // If no duplicates were found after checking all elements
+    return false;
+};
