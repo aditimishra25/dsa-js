f0ab3be2c1db742556bcbe9a677881f7f0f9f53f
Aditi Mishra <aditimishra2501@gmail.com>
2026-03-10 14:16:20 +0530

    binary tree right side view

---
+++ tree/binary-tree-right-side-view.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+    let res = [];
+    checkLevel(root, 0, res);
+    return res;
+};
+
+// var checkLevel = (root, level, res) => {
+//     if (!root) return;
+
+//     if(res[level] === undefined) res[level] = root.val;
+
+//     checkLevel(root.right, level+1, res)
+//     checkLevel(root.left, level+1, res)
+// }
+
+//using queue
+var checkLevel = (root, level, res) => {
+    if (!root) return;
+    let queue = [root];
+    while (queue.length) {
+        let size = queue.length
+        for (let i = 0; i < size; i++) {
+
+            let node = queue.shift();
+
+            if (i === size - 1) {
+                res.push(node.val);
+            }
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+    return res;
+}
