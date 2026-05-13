25bee6439337dd7c88f3e68407cc9ca910e67dd7
Aditi Mishra <aditimishra2501@gmail.com>
2026-05-13 14:21:54 +0530

    kadane's algorithm: max subarray

--- dynamic programming/kadane's algorithm/maximum-subarray.js
+++ dynamic programming/kadane's algorithm/maximum-subarray.js
@@ -2,6 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
+  //init max sum to -infinity and current sum to 0
+  let maxSoFar = -Infinity,
+    currentMax = 0;
 
+  for (num of nums) {
+    // if current sum is less than 0, it implies that negative numbers were found along the way and hence create a new subarray
+    if (currentMax < 0) currentMax = 0;
+
+    // increment current max
+    currentMax += nums[i];
+
+    // set max sum so far
+    if (currentMax > maxSoFar) maxSoFar = currentMax;
+  }
+
+  //return max sum
+  return maxSoFar;
 };
