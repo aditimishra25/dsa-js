3823d52ae94b1d262bd94314a5226b939d874f32
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-02 14:55:55 +0530

    max points from cards

--- sliding-window/fixed size/maximum-points-you-can-obtain-from-cards.js
+++ sliding-window/fixed size/maximum-points-you-can-obtain-from-cards.js
@@ -4,5 +4,36 @@
  * @return {number}
  */
 var maxScore = function(cardPoints, k) {
-    
+    let total = cardPoints.reduce((sum, num) => sum + num, 0);
+
+    if (cardPoints.length === k) {
+        return total;
+    }
+
+    let windowSize = cardPoints.length - k;
+
+    let windowSum = 0;
+
+    for (let i = 0; i < windowSize; i++) {
+        windowSum += cardPoints[i];
+    }
+
+    let minWindowSum = windowSum;
+
+    let left = 0;
+
+    for (let right = windowSize; right < cardPoints.length; right++) {
+
+        // Add new element entering window
+        windowSum += cardPoints[right];
+
+        // Remove old element leaving window
+        windowSum -= cardPoints[left];
+
+        left++;
+
+        minWindowSum = Math.min(minWindowSum, windowSum);
+    }
+
+    return total - minWindowSum;
 };
