7c74af9f67be593cf89f05a9dec4f32310c2bbbc
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-16 14:26:45 +0530

    invert bst

--- tree/dfs/invert-binary-tree.js
+++ tree/dfs/invert-binary-tree.js
@@ -27,3 +27,17 @@ var invertTree = function(root) {
     // Return the inverted root node.
     return root;
 };
+
+// ---------------------revision-1---------------------------------
+var invertTree = function(root) {
+    if(!root) return null;
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root;
+};
