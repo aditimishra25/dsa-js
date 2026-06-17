adb8d9ebba6d65af397dc027fed9ae40e77c9567
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-17 15:22:37 +0530

    revision: tree contsruction

--- tree/tree-construction/construct-binary-tree-from-preorder-and-postorder-traversal.js
+++ tree/tree-construction/construct-binary-tree-from-preorder-and-postorder-traversal.js
@@ -12,40 +12,86 @@
  * @return {TreeNode}
  */
 
+// Find root from preorder
+
+// Take next preorder value
+// ↓
+// that's left subtree root
+
+// Find it in postorder
+// ↓
+// calculate left subtree size
+
+// Use size to split
+
 let preIndex = 0;
 let map = new Map();
 
-var constructFromPrePost = function(preorder, postorder) {
-    map.clear();
+var constructFromPrePost = function (preorder, postorder) {
+  map.clear();
 
-    for (let i = 0; i < postorder.length; i++) {
-        map.set(postorder[i], i);
-    }
+  for (let i = 0; i < postorder.length; i++) {
+    map.set(postorder[i], i);
+  }
 
-    preIndex = 0;
+  preIndex = 0;
 
-    return dfs(preorder, postorder, 0, postorder.length - 1);
+  return dfs(preorder, postorder, 0, postorder.length - 1);
 };
 
 function dfs(preorder, postorder, left, right) {
-    if (left > right) return null;
+  if (left > right) return null;
 
-    let root = new TreeNode(preorder[preIndex++]);
+  let root = new TreeNode(preorder[preIndex++]);
 
-    // if single node
-    if (left === right) return root;
+  // if single node
+  if (left === right) return root;
 
-    // next preorder is left child
-    let leftChild = preorder[preIndex];
+  // next preorder is left child
+  let leftChild = preorder[preIndex];
 
-    // find in postorder
-    let index = map.get(leftChild);
+  // find in postorder
+  let index = map.get(leftChild);
 
-    // build left subtree
-    root.left = dfs(preorder, postorder, left, index);
+  // build left subtree
+  root.left = dfs(preorder, postorder, left, index);
 
-    // build right subtree
-    root.right = dfs(preorder, postorder, index + 1, right - 1);
+  // build right subtree
+  root.right = dfs(preorder, postorder, index + 1, right - 1);
 
-    return root;
+  return root;
 }
+
+// ----------------------revision-1------------------------------------------
+let preIndex = 0;
+let map = new Map();
+
+var constructFromPrePost = function (preorder, postorder) {
+  map.clear();
+
+  for (let i = 0; i < postorder.length; i++) {
+    map.set(postorder[i], i);
+  }
+
+  preIndex = 0;
+
+  return dfs(preorder, postorder, 0, postorder.length - 1);
+};
+
+var dfs = (preorder, postorder, left, right) => {
+  if (left > right) return null;
+
+  let rootVal = preorder[preIndex++];
+  let root = new TreeNode(rootVal);
+
+  if (left === right) return root;
+
+  let leftChild = preorder[preIndex];
+
+  let index = map.get(leftChild);
+
+  root.left = dfs(preorder, postorder, left, index);
+  root.right = dfs(preorder, postorder, index + 1 , right - 1);
+
+  return root;
+};
