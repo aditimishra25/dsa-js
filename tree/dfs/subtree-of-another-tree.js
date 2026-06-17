1fb86ba03c9f2c375e1e06e77b59f81c666701d6
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-17 10:12:04 +0530

    subtree of another tree

---
+++ tree/dfs/subtree-of-another-tree.js
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val === undefined ? 0 : val)
+ *     this.left = (left === undefined ? null : left)
+ *     this.right = (right === undefined ? null : right)
+ * }
+ */
+
+var isSubtree = function(root, subRoot) {
+    // If we've reached the end of root,
+    // subRoot cannot be found here
+    if (!root) {
+        return false;
+    }
+
+    // Check if the tree rooted at the current node
+    // is exactly the same as subRoot
+    if (sameTree(root, subRoot)) {
+        return true;
+    }
+
+    // Otherwise search in the left subtree
+    // or the right subtree
+    return (
+        isSubtree(root.left, subRoot) ||
+        isSubtree(root.right, subRoot)
+    );
+};
+
+var sameTree = (rootTree, subRootTree) => {
+    // Both nodes are null
+    // Trees match up to this point
+    if (!rootTree && !subRootTree) {
+        return true;
+    }
+
+    // One node is null
+    // OR values are different
+    // Trees are not identical
+    if (
+        !rootTree ||
+        !subRootTree ||
+        rootTree.val !== subRootTree.val
+    ) {
+        return false;
+    }
+
+    // Both current nodes match
+    // Check left and right subtrees
+    return (
+        sameTree(rootTree.left, subRootTree.left) &&
+        sameTree(rootTree.right, subRootTree.right)
+    );
+};
+
+
+// root
+//  |
+//  v
+
+// Is this subtree equal
+// to subRoot?
+
+// If yes:
+// return true
+
+// If no:
+// Search left subtree
+// Search right subtree
