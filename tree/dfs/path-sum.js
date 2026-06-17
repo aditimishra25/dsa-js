a0d4832dd105e194d36f32d30a4820bbec94cbd7
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-17 10:50:29 +0530

    revision: path sum

--- tree/dfs/path-sum.js
+++ tree/dfs/path-sum.js
@@ -23,3 +23,15 @@ var hasPathSum = function (root, targetSum) {
     return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
 
 };
+
+// -------------------revision-1-------------------------------------
+var hasPathSum = function (root, targetSum) {
+    if(!root) return false;
+
+    if(!root.left && !root.right) return targetSum === root.val
+    
+    targetSum -= root.val;
+
+    return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum)
+
+};
