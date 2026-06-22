191c5ca4bf7d3933bcdbbe4eb2b26623337f9823
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-22 17:50:26 +0530

    word ladder

---
+++ graphs/bfs/word-ladder.js
@@ -0,0 +1,71 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+
+    // Fast lookup
+    let words = new Set(wordList);
+
+    // If target doesn't exist, impossible
+    if (!words.has(endWord)) {
+        return 0;
+    }
+
+    // BFS queue
+    let queue = [beginWord];
+
+    // Prevent revisiting words
+    let visited = new Set();
+    visited.add(beginWord);
+
+    // Current transformation length
+    let level = 1;
+
+    while (queue.length) {
+
+        let size = queue.length;
+
+        // Process one BFS level
+        for (let i = 0; i < size; i++) {
+
+            let word = queue.shift();
+
+            // Reached target
+            if (word === endWord) {
+                return level;
+            }
+
+            // Try changing every character
+            for (let j = 0; j < word.length; j++) {
+
+                // Convert to array because strings are immutable
+                let chars = word.split("");
+
+                // Try all 26 letters
+                for (let c = 97; c <= 122; c++) {
+
+                    chars[j] = String.fromCharCode(c);
+
+                    let nextWord = chars.join("");
+
+                    // Valid dictionary word and not visited
+                    if (
+                        words.has(nextWord) &&
+                        !visited.has(nextWord)
+                    ) {
+                        visited.add(nextWord);
+                        queue.push(nextWord);
+                    }
+                }
+            }
+        }
+
+        // Move to next BFS level
+        level++;
+    }
+
+    return 0;
+};
