768110e3da5c7fc65b59b17009ba65ead2550357
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-22 11:54:33 +0530

    revision: lowest common ancestor of a binary tree

---
+++ tree/dfs/lowest-common-ancestor-of-binary-tree.js
@@ -0,0 +1,12 @@
+var lowestCommonAncestor = function(root, p, q) {
+
+    if(!root || root === p || root === q) return root;
+
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+
+    if(left === null) return right;
+    if(right === null) return left;
+    return root
+    
+};
