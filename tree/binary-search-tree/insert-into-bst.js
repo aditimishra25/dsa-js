69be98b217a4828cddd7b9f4884e67631ad1f66b
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-22 12:21:35 +0530

    insert into bst

---
+++ tree/binary-search-tree/insert-into-bst.js
@@ -0,0 +1,14 @@
+var insertIntoBST = function(root, val) {
+    if (!root) {
+        return new TreeNode(val);
+    }
+
+    if(val < root.val){
+        root.left = insertIntoBST(root.left, val)
+    }
+    if(val > root.val){
+        root.right = insertIntoBST(root.right, val)
+    }
+
+    return root;
+};
