1fa90d7a202e5a3849668f60b2cad2761728899d
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-22 11:45:29 +0530

    revision: lowest common ancestor

--- tree/binary-search-tree/lowest-common-ancestor.js
+++ tree/binary-search-tree/lowest-common-ancestor.js
@@ -14,13 +14,18 @@
  */
 var lowestCommonAncestor = function (root, p, q) {
   // Start at the root of the BST.
-  
+
   // If both p and q are larger than the root, the LCA must be in the right subtree.
   if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
-  
   // If both p and q are smaller than the root, the LCA must be in the left subtree.
   else if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
-  
   // If p and q are on different sides of the root (or one equals the root), the current node is the LCA.
   else return root;
+};
+
+// ---------------------revision-1-----------------------------------------------
+var lowestCommonAncestor = function (root, p, q) {
+  if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+  if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+  return root;
 };
