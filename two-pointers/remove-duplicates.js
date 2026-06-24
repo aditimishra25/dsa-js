2fe3d0de9763b0bfb852de2a49c0bcfc6d2df6e4
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-24 14:53:41 +0530

    revision: 2 pointers

--- two-pointers/remove-duplicates.js
+++ two-pointers/remove-duplicates.js
@@ -3,15 +3,29 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    let j = 1;
-    if (nums.length) {
-        for (let i = 1; i < nums.length; i++) {
-            if (nums[i] !== nums[i - 1]){
-                nums[j] = nums[i]
-                j++
-            }
-        }
-        return j;
+  let j = 1;
+  if (nums.length) {
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i] !== nums[i - 1]) {
+        nums[j] = nums[i];
+        j++;
+      }
     }
-    return 0;
+    return j;
+  }
+  return 0;
+};
+
+//----------------------------revision-1-------------------------------------
+var removeDuplicates = function (nums) {
+  let right = 1;
+
+  if (!nums.length) return 0;
+  for (let left = 1; left < nums.length; left++) {
+    if (nums[left] != nums[left - 1]) {
+      nums[right] = nums[left];
+      right++;
+    }
+  }
+  return right;
 };
