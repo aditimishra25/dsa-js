be91dd9f221da30389b29ad970bca56645f352e0
Aditi Mishra <aditimishra2501@gmail.com>
2026-06-30 17:44:59 +0530

    revision: sliding window

--- sliding-window/variable size/fruits-into-basket.js
+++ sliding-window/variable size/fruits-into-basket.js
@@ -2,43 +2,62 @@
  * @param {number[]} fruits
  * @return {number}
  */
-var totalFruit = function(fruits) {
+var totalFruit = function (fruits) {
+  // left pointer of sliding window
+  let left = 0;
 
-    // left pointer of sliding window
-    let left = 0;
+  // stores maximum window size found
+  let maxNum = 0;
 
-    // stores maximum window size found
-    let maxNum = 0;
+  // hashmap to store fruit counts
+  let varCount = {};
 
-    // hashmap to store fruit counts
-    let varCount = {}
+  // expand window using right pointer
+  for (let right = 0; right < fruits.length; right++) {
+    // add current fruit into hashmap
+    varCount[fruits[right]] = (varCount[fruits[right]] || 0) + 1;
 
-    // expand window using right pointer
-    for(let right = 0; right < fruits.length; right++) {
+    // if more than 2 fruit types exist
+    while (Object.keys(varCount).length > 2) {
+      // remove left fruit count
+      varCount[fruits[left]]--;
 
-        // add current fruit into hashmap
-        varCount[fruits[right]] =
-            (varCount[fruits[right]] || 0) + 1;
+      // if count becomes 0
+      // remove fruit type completely
+      if (varCount[fruits[left]] == 0) {
+        delete varCount[fruits[left]];
+      }
 
-        // if more than 2 fruit types exist
-        while(Object.keys(varCount).length > 2) {
-
-            // remove left fruit count
-            varCount[fruits[left]]--;
-
-            // if count becomes 0
-            // remove fruit type completely
-            if(varCount[fruits[left]] == 0) {
-                delete varCount[fruits[left]];
-            }
-
-            // shrink window
-            left++;
-        }
-
-        // calculate current valid window size
-        maxNum = Math.max(maxNum, right - left + 1);
+      // shrink window
+      left++;
     }
 
-    return maxNum;
+    // calculate current valid window size
+    maxNum = Math.max(maxNum, right - left + 1);
+  }
+
+  return maxNum;
+};
+
+// -------------------------revision-1------------------------------------------
+//window size is 2
+var totalFruit = function (fruits) {
+  let left = 0;
+  let maxFruits = 0;
+  let varCount = {};
+
+  for (let right = 0; right < fruits.length; right++) {
+    varCount[fruits[right]] = (varCount[fruits[right]] || 0) + 1;
+
+    while (Object.keys(varCount).length > 2) {
+      varCount[fruits[left]] --;
+      if (varCount[fruits[left]] == 0) {
+        delete varCount[fruits[left]];
+      }
+      left++;
+    }
+    maxFruits = Math.max(maxFruits, right - left + 1);
+  }
+  console.log(varCount);
+  return maxFruits;
 };
--- sliding-window/variable size/longest-substring.js
+++ sliding-window/variable size/longest-substring.js
@@ -12,48 +12,62 @@ intuition:
         are ever within the window. 
 */
 var lengthOfLongestSubstring = function (s) {
-    let left = 0
-    let maxLength = 0;
-    let unique = new Set();
+  let left = 0;
+  let maxLength = 0;
+  let unique = new Set();
 
-    for (let right = 0; right < s.length; right++) {
-        while (unique.has(s.charAt(right))) {
-            unique.delete(s.charAt(left))
-            left++;
-        }
-        unique.add(s.charAt(right))
-        maxLength = Math.max(maxLength, right - left + 1)
+  for (let right = 0; right < s.length; right++) {
+    while (unique.has(s.charAt(right))) {
+      unique.delete(s.charAt(left));
+      left++;
     }
+    unique.add(s.charAt(right));
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
 
-    return maxLength;
-}
+  return maxLength;
+};
 
 // --------revision-1--------------------
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
+  let left = 0;
+  let maxLength = 0;
 
-    let left = 0;
-    let maxLength = 0;
+  let set = new Set();
 
-    let set = new Set();
+  for (let right = 0; right < s.length; right++) {
+    // shrink window until duplicate removed
+    while (set.has(s[right])) {
+      // remove left character
+      set.delete(s[left]);
 
-    for(let right = 0; right < s.length; right++) {
-
-        // shrink window until duplicate removed
-        while(set.has(s[right])) {
-
-            // remove left character
-            set.delete(s[left]);
-
-            // move left pointer
-            left++;
-        }
-
-        // add current character
-        set.add(s[right]);
-
-        // update max length
-        maxLength = Math.max(maxLength, right - left + 1);
+      // move left pointer
+      left++;
     }
 
-    return maxLength;
+    // add current character
+    set.add(s[right]);
+
+    // update max length
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+};
+
+// -------------------------revision-2------------------------------------------
+var lengthOfLongestSubstring = function (s) {
+  let left = 0;
+  let maxLength = 0;
+  let uniqueChars = new Set();
+
+  for (let right = 0; right < s.length; right++) {
+    while (uniqueChars.has(s[right])) {
+      uniqueChars.delete(s[left]);
+      left++;
+    }
+    uniqueChars.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+  return maxLength;
 };
