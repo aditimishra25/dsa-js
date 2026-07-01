eb240d090fcb4dfc037c774021d21176c62c7ec3
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-01 13:41:51 +0530

    revision: sliding window

--- sliding-window/variable size/longest-repeating-character-replacement.js
+++ sliding-window/variable size/longest-repeating-character-replacement.js
@@ -3,43 +3,62 @@
  * @param {number} k
  * @return {number}
  */
-var characterReplacement = function(s, k) {
+var characterReplacementlast = function (s, k) {
+  let count = {};
 
-    let count = {};
+  let left = 0;
 
-    let left = 0;
+  // stores highest frequency character in current window
+  let maxFreq = 0;
 
-    // stores highest frequency character in current window
-    let maxFreq = 0;
+  // stores answer
+  let maxLength = 0;
 
-    // stores answer
-    let maxLength = 0;
+  for (let right = 0; right < s.length; right++) {
+    // add current character frequency
+    count[s[right]] = (count[s[right]] || 0) + 1;
 
-    for(let right = 0; right < s.length; right++) {
+    // update highest frequency seen in window
+    maxFreq = Math.max(maxFreq, count[s[right]]);
 
-        // add current character frequency
-        count[s[right]] = (count[s[right]] || 0) + 1;
+    /**
+     * replacements needed:
+     * window size - most frequent char
+     */
+    while (right - left + 1 - maxFreq > k) {
+      count[s[left]]--;
 
-        // update highest frequency seen in window
-        maxFreq = Math.max(maxFreq, count[s[right]]);
-
-        /**
-         * replacements needed:
-         * window size - most frequent char
-         */
-        while((right - left + 1) - maxFreq > k) {
-
-            count[s[left]]--;
-
-            left++;
-        }
-
-        // update answer
-        maxLength = Math.max(
-            maxLength,
-            right - left + 1
-        );
+      left++;
     }
 
-    return maxLength;
+    // update answer
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
 };
+
+// -------------------------revision-1------------------------------------------
+var characterReplacement = function (s, k) {
+  let maxLength = 0;
+  let count = {};
+  let left = 0;
+  let maxFreq = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    count[s[right]] = (count[s[right]] || 0) + 1;
+
+    maxFreq = Math.max(maxFreq, count[s[right]]);
+    
+    // How many characters must be replaced to make the entire window consist of the most frequent character.
+    // If that number exceeds k, the window is invalid.
+    while (right - left + 1 - maxFreq > k) {
+      count[s[left]]--;
+      left++;
+    }
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+  return maxLength;
+};
+
+characterReplacement("ABAB", 2); //4
