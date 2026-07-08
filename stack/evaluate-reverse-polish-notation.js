24aebcb270b970bd11514b90ce7f54611776368e
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-08 12:29:51 +0530

    revision: stack

--- stack/evaluate-reverse-polish-notation.js
+++ stack/evaluate-reverse-polish-notation.js
@@ -2,51 +2,77 @@
  * @param {string[]} tokens
  * @return {number}
  */
-var evalRPN = function(tokens) {
+var evalRPN = function (tokens) {
+  let stack = [];
 
-    let stack = [];
+  for (let i = 0; i < tokens.length; i++) {
+    if (isNumber(tokens[i])) {
+      stack.push(Number(tokens[i]));
+    } else {
+      let b = stack.pop();
+      let a = stack.pop();
 
-    for (let i = 0; i < tokens.length; i++) {
+      let result = performOperations(a, b, tokens[i]);
 
-        if (isNumber(tokens[i])) {
-
-            stack.push(Number(tokens[i]));
-
-        } else {
-
-            let b = stack.pop();
-            let a = stack.pop();
-
-            let result = performOperations(a, b, tokens[i]);
-
-            stack.push(result);
-        }
+      stack.push(result);
     }
+  }
 
-    return stack[0];
+  return stack[0];
 };
 
 function isNumber(str) {
-    return typeof str === 'string' &&
-           str.trim() !== '' &&
-           !isNaN(Number(str));
+  return typeof str === "string" && str.trim() !== "" && !isNaN(Number(str));
 }
 
 function performOperations(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return a + b;
 
-    switch (operator) {
+    case "-":
+      return a - b;
 
-        case "+":
-            return a + b;
+    case "*":
+      return a * b;
 
-        case "-":
-            return a - b;
-
-        case "*":
-            return a * b;
-
-        case "/":
-            return Math.trunc(a / b);
-    }
+    case "/":
+      return Math.trunc(a / b);
+  }
 }
 
+// ------------------------revision-1---------------------------------
+var evalRPN = function (tokens) {
+  let stack = [];
+  for (let i = 0; i < tokens.length; i++) {
+    if (isNumber(tokens[i])) {
+      stack.push(Number(tokens[i]));
+    } else {
+      let a = stack.pop();
+      let b = stack.pop();
+
+      stack.push(performOperations(b, a, tokens[i]));
+    }
+  }
+  console.log(stack);
+  return stack[0];
+};
+
+isNumber = (str) => {
+  return typeof str === "string" && str.trim() !== "" && !isNaN(Number(str));
+};
+
+performOperations = (num1, num2, operator) => {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+  }
+};
+
+evalRPN(["2", "1", "+", "3", "*"]);
