ad28c74728c1a21cf95a1ba5356206573c75a470
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-09 17:17:36 +0530

    revision: monotonic stack

--- stack/monotonic-stack/largest-rectangle-in-histogram.js
+++ stack/monotonic-stack/largest-rectangle-in-histogram.js
@@ -5,58 +5,68 @@
  *
  * Stack stores indices.
  */
-var largestRectangleArea = function(heights) {
+var largestRectangleArea = function (heights) {
+  let area = 0;
 
-    let area = 0;
+  let stack = [];
 
-    let stack = [];
+  for (let i = 0; i < heights.length; i++) {
+    // Current bar is smaller,
+    // so previous taller bars cannot extend further right.
+    while (stack.length > 0 && heights[stack[stack.length - 1]] > heights[i]) {
+      let poppedIndex = stack.pop();
 
-    for (let i = 0; i < heights.length; i++) {
+      let height = heights[poppedIndex];
 
-        // Current bar is smaller,
-        // so previous taller bars cannot extend further right.
-        while (
-            stack.length > 0 &&
-            heights[stack[stack.length - 1]] > heights[i]
-        ) {
+      let right = i;
 
-            let poppedIndex = stack.pop();
+      let left = stack.length === 0 ? -1 : stack[stack.length - 1];
 
-            let height = heights[poppedIndex];
+      let width = right - left - 1;
 
-            let right = i;
-
-            let left =
-                stack.length === 0
-                    ? -1
-                    : stack[stack.length - 1];
-
-            let width = right - left - 1;
-
-            area = Math.max(area, height * width);
-        }
-
-        stack.push(i);
+      area = Math.max(area, height * width);
     }
 
-    // Process remaining bars
-    while (stack.length > 0) {
+    stack.push(i);
+  }
 
-        let poppedIndex = stack.pop();
+  // Process remaining bars
+  while (stack.length > 0) {
+    let poppedIndex = stack.pop();
 
-        let height = heights[poppedIndex];
+    let height = heights[poppedIndex];
 
-        let right = heights.length;
+    let right = heights.length;
 
-        let left =
-            stack.length === 0
-                ? -1
-                : stack[stack.length - 1];
+    let left = stack.length === 0 ? -1 : stack[stack.length - 1];
 
-        let width = right - left - 1;
+    let width = right - left - 1;
 
-        area = Math.max(area, height * width);
+    area = Math.max(area, height * width);
+  }
+
+  return area;
+};
+
+// ------------------------revision-1---------------------------------
+var largestRectangleArea = function (heights) {
+  let maxArea = 0;
+  let stack = [];
+
+  for (let i = 0; i <= heights.length; i++) {
+    let currHeight = i === heights.length ? 0 : heights[i];
+    while (stack.length && currHeight < heights[stack[stack.length - 1]]) {
+      let height = heights[stack.pop()];
+      let right = i;
+      let left = stack.length ? stack[stack.length - 1] : -1;
+
+      let width = right - left - 1;
+
+      maxArea = Math.max(maxArea, height * width);
     }
 
-    return area;
+    stack.push(i);
+  }
+
+  return maxArea;
 };
