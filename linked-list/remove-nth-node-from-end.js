baceef21961a4bec504cf2c9972cb9b3a0fab101
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-13 11:20:08 +0530

    revision: linked list

--- linked-list/remove-nth-node-from-end.js
+++ linked-list/remove-nth-node-from-end.js
@@ -1,30 +1,29 @@
 /**
  * Remove Nth Node From End
  */
-var removeNthFromEnd = function(head, n) {
+var removeNthFromEnd = function (head, n) {
+  // Dummy handles edge cases like removing head
+  let dummy = new ListNode(0);
+  dummy.next = head;
 
-    // Dummy handles edge cases like removing head
-    let dummy = new ListNode(0);
-    dummy.next = head;
+  let slow = dummy;
+  let fast = dummy;
 
-    let slow = dummy;
-    let fast = dummy;
+  // Move fast n steps ahead
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
 
-    // Move fast n steps ahead
-    for (let i = 0; i < n; i++) {
-        fast = fast.next;
-    }
+  // Move both until fast reaches last node
+  while (fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
 
-    // Move both until fast reaches last node
-    while (fast.next !== null) {
-        slow = slow.next;
-        fast = fast.next;
-    }
+  // Remove node after slow
+  slow.next = slow.next.next;
 
-    // Remove node after slow
-    slow.next = slow.next.next;
-
-    return dummy.next;
+  return dummy.next;
 };
 
 // Why does this work?
@@ -35,3 +34,51 @@ var removeNthFromEnd = function(head, n) {
 // slow will be immediately before
 // the nth node from the end.
 // That's why this is called a fixed-gap two-pointer problem. The gap never changes after we create it.
+
+// ----------------revision-1--------------------------
+// ----Approach-1(brute force)
+var removeNthFromEnd = function (head, n) {
+  let length = 0;
+  let curr = head;
+
+  while (curr) {
+    length++;
+    curr = curr.next;
+  }
+
+  if (length == n) return head.next;
+
+  let positionToDeleteFromStart = length - n;
+
+  curr = head;
+  for (let i = 1; i < positionToDeleteFromStart; i++) {
+    curr = curr.next;
+  }
+
+  curr.next = curr.next.next;
+
+  return head;
+};
+
+// ----Approach-1(optimal)
+var removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+
+  let slow = dummy;
+  let fast = dummy;
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+
+  while (fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  // Remove node after slow
+  slow.next = slow.next.next;
+
+  return dummy.next;
+};
