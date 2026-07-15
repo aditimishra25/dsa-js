84955242001931b00c92c5b16a3a97f234c55872
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-15 16:02:48 +0530

    revision: tree bfs

--- tree/bfs/average-of-levels-in-binary-tree.js
+++ tree/bfs/average-of-levels-in-binary-tree.js
@@ -11,19 +11,65 @@
  * @return {number[]}
  */
 var averageOfLevels = function (root) {
-    let res = [];
-    bfs(root, 0, res);
-    return res.map(([sum, count]) => sum / count);
+  let res = [];
+  bfs(root, 0, res);
+  return res.map(([sum, count]) => sum / count);
 };
 
 var bfs = (root, level, res) => {
-    if (!root) return;
+  if (!root) return;
 
-    if (!res[level]) res[level] = [0, 0]; // [sum, count]
+  if (!res[level]) res[level] = [0, 0]; // [sum, count]
 
-    res[level][0] += root.val; // add to sum
-    res[level][1] += 1;        // increase count
+  res[level][0] += root.val; // add to sum
+  res[level][1] += 1; // increase count
 
-    bfs(root.left, level + 1, res);
-    bfs(root.right, level + 1, res);
+  bfs(root.left, level + 1, res);
+  bfs(root.right, level + 1, res);
+};
+
+// ----------------------revision-1---------------------------
+// Approach-1
+var averageOfLevels = function (root) {
+  let res = [];
+  bfs(root, 0, res);
+  return res.map(([sum, count]) => sum / count);
+};
+
+bfs = (root, level, res) => {
+  if (!root) return;
+
+  if (!res[level]) res[level] = [0, 0]; // [sum, count]
+
+  res[level][0] += root.val; // add to sum
+  res[level][1] += 1; // increase count
+
+  bfs(root.left, level + 1, res);
+  bfs(root.right, level + 1, res);
+};
+
+// Approach-2
+var averageOfLevels = function (root) {
+   if (!root) return [];
+
+    let result = [];
+    let queue = [root];
+
+    while(queue.length){
+        let level = [];
+        let size = queue.length;
+        let sum = 0;
+
+        for(let i=0; i<size; i++){
+            let node = queue.shift();
+            level.push(node.val);
+
+            sum = sum + node.val
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right)
+        }
+
+        result.push(sum/size)
+    }
+    return result
 };
--- tree/bfs/binary-tree-level-order-traversal.js
+++ tree/bfs/binary-tree-level-order-traversal.js
@@ -11,56 +11,80 @@
  * @return {number[][]}
  */
 var levelOrder = function (root) {
-    let res = [];
-    bfs(root, 0, res);
-    return res;
+  let res = [];
+  bfs(root, 0, res);
+  return res;
 };
 
 var bfs = (root, level, res) => {
-    if (!root) return;
+  if (!root) return;
 
-    // create array for this level if it doesn't exist
-    if (!res[level]) {
-        res[level] = [];
-    }
+  // create array for this level if it doesn't exist
+  if (!res[level]) {
+    res[level] = [];
+  }
 
-    // add node value to correct level
-    res[level].push(root.val);
+  // add node value to correct level
+  res[level].push(root.val);
 
-    // visit children
-    bfs(root.left, level + 1, res);
-    bfs(root.right, level + 1, res);
+  // visit children
+  bfs(root.left, level + 1, res);
+  bfs(root.right, level + 1, res);
 };
 
 // ---------------------------revision-1-------------------------------------------
-var levelOrder = function(root) {
-    if (!root) return [];
+var levelOrder = function (root) {
+  if (!root) return [];
 
-    let result = [];
-    let queue = [root];
+  let result = [];
+  let queue = [root];
 
-    while (queue.length) {
-        let level = [];
+  while (queue.length) {
+    let level = [];
 
-        // Save current level size
-        let levelSize = queue.length;
+    // Save current level size
+    let levelSize = queue.length;
 
-        for (let i = 0; i < levelSize; i++) {
-            let node = queue.shift();
+    for (let i = 0; i < levelSize; i++) {
+      let node = queue.shift();
 
-            level.push(node.val);
+      level.push(node.val);
 
-            if (node.left) {
-                queue.push(node.left);
-            }
+      if (node.left) {
+        queue.push(node.left);
+      }
 
-            if (node.right) {
-                queue.push(node.right);
-            }
-        }
-
-        result.push(level);
+      if (node.right) {
+        queue.push(node.right);
+      }
     }
 
-    return result;
+    result.push(level);
+  }
+
+  return result;
+};
+
+// -------------------revision-2---------------------------
+var levelOrder = function (root) {
+  if (!root) return [];
+
+  let result = [];
+  let queue = [root];
+
+  while (queue.length) {
+    let level = [];
+    let size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+
+      level.push(node.val);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(level);
+  }
+  return result;
 };
