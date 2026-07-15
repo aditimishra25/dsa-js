ed0b9b6717c1050ed5be17ecaff6a2cc83b41651
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-15 11:05:21 +0530

    revision: tree dfs

--- tree/dfs/lowest-common-ancestor-of-binary-tree.js
+++ tree/dfs/lowest-common-ancestor-of-binary-tree.js
@@ -1,12 +1,23 @@
-var lowestCommonAncestor = function(root, p, q) {
+var lowestCommonAncestor = function (root, p, q) {
+  if (!root || root === p || root === q) return root;
 
-    if(!root || root === p || root === q) return root;
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
 
-    let left = lowestCommonAncestor(root.left, p, q);
-    let right = lowestCommonAncestor(root.right, p, q);
+  if (left === null) return right;
+  if (right === null) return left;
+  return root;
+};
 
-    if(left === null) return right;
-    if(right === null) return left;
-    return root
-    
+// -------------------------revision-1------------------------------
+var lowestCommonAncestor = function (root, p, q) {
+  if (!root || p == root || q == root) return root;
+
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+
+  if (left == null) return right;
+  if (right == null) return left;
+
+  return root;
 };
