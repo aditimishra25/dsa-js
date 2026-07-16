795872003359a16dbdef49f505ca165fda1a9830
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-16 12:19:45 +0530

    revision: tree bst

--- tree/binary-search-tree/convert-sorted-array-to-binary-search-tree.js
+++ tree/binary-search-tree/convert-sorted-array-to-binary-search-tree.js
@@ -11,46 +11,60 @@
  * @return {TreeNode}
  */
 var sortedArrayToBST = function (nums) {
-    return convertToBST(nums, 0, nums.length - 1)
+  return convertToBST(nums, 0, nums.length - 1);
 };
 
 //function to convert sorted array to bst
 var convertToBST = (nums, start, end) => {
-    //return null when wrong range received
-    if (start > end) return null;
+  //return null when wrong range received
+  if (start > end) return null;
 
-    //calculate the midlle element as this is node
-    let mid = Math.ceil((start + end) / 2);
+  //calculate the midlle element as this is node
+  let mid = Math.ceil((start + end) / 2);
 
-    //initialise root node as middle element
-    var root = new TreeNode(nums[mid]);
+  //initialise root node as middle element
+  var root = new TreeNode(nums[mid]);
 
-    //assign left subtree
-    root.left = convertToBST(nums, start, mid - 1);
+  //assign left subtree
+  root.left = convertToBST(nums, start, mid - 1);
 
-    //assign right subtree
-    root.right = convertToBST(nums, mid + 1, end);
+  //assign right subtree
+  root.right = convertToBST(nums, mid + 1, end);
 
-    //return root node
-    return root;
-
-}
+  //return root node
+  return root;
+};
 
 // -----------------revision-1---------------------------------------
-var sortedArrayToBST = function(nums) {
-    return convertArrayToBST(nums, 0, nums.length - 1)
+var sortedArrayToBST = function (nums) {
+  return convertArrayToBST(nums, 0, nums.length - 1);
 };
 
 convertArrayToBST = (nums, start, end) => {
-    if(start > end) return null;
+  if (start > end) return null;
 
-    let mid = Math.ceil(start + (end - start)/2);
+  let mid = Math.ceil(start + (end - start) / 2);
 
-    let root = new TreeNode(nums[mid]);
+  let root = new TreeNode(nums[mid]);
 
-    root.left = convertArrayToBST(nums, start, mid - 1);
-    root.right = convertArrayToBST(nums, mid+1, end)
+  root.left = convertArrayToBST(nums, start, mid - 1);
+  root.right = convertArrayToBST(nums, mid + 1, end);
 
-    return root;
+  return root;
+};
 
-}
+// -----------------------revision-2--------------------------
+var sortedArrayToBST = function (nums) {
+  return convertArrayToBST(nums, 0, nums.length - 1);
+};
+
+convertArrayToBST = (nums, start, end) => {
+  if (start > end) return null;
+
+  let mid = Math.ceil(start + (end - start) / 2);
+
+  let root = new TreeNode(nums[mid]);
+  root.left = convertArrayToBST(nums, start, mid - 1);
+  root.right = convertArrayToBST(nums, mid + 1, end);
+  return root;
+};
--- tree/binary-search-tree/insert-into-bst.js
+++ tree/binary-search-tree/insert-into-bst.js
@@ -1,14 +1,22 @@
-var insertIntoBST = function(root, val) {
-    if (!root) {
-        return new TreeNode(val);
-    }
+var insertIntoBST = function (root, val) {
+  if (!root) {
+    return new TreeNode(val);
+  }
 
-    if(val < root.val){
-        root.left = insertIntoBST(root.left, val)
-    }
-    if(val > root.val){
-        root.right = insertIntoBST(root.right, val)
-    }
+  if (val < root.val) {
+    root.left = insertIntoBST(root.left, val);
+  }
+  if (val > root.val) {
+    root.right = insertIntoBST(root.right, val);
+  }
 
-    return root;
+  return root;
+};
+
+// -----------------------revision-1--------------------------
+var insertIntoBST = function (root, val) {
+  if (!root) return new TreeNode(val);
+  if (val < root.val) root.left = insertIntoBST(root.left, val);
+  if (val > root.val) root.right = insertIntoBST(root.right, val);
+  return root;
 };
