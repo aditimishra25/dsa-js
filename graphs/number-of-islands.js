223c32d6f95f4cfcdd36d248030e89520749bbab
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-17 10:19:29 +0530

    graphs: nos of islands

--- graphs/number-of-islands.js
+++ graphs/number-of-islands.js
@@ -12,24 +12,24 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
-    let islands = 0
-   for (let r = 0; r < grid.length; r++) {
+var numIslands = function (grid) {
+  let islands = 0;
+  for (let r = 0; r < grid.length; r++) {
     for (let c = 0; c < grid[0].length; c++) {
-        if(grid[r][c] == "1") {
-            islands++
-            dfs(grid, r, c)
-        }   
-        }
+      if (grid[r][c] == "1") {
+        islands++;
+        dfs(grid, r, c);
+      }
     }
-    return islands;
+  }
+  return islands;
 };
 
 var dfs = (grid, row, col) => {
-    if(row<0 || col <0 || row >= grid.length || col >= grid[0].length || grid[row][col] === "0") return;
-    grid[row][col] = "0"
-    dfs(grid, row - 1, col) //up
-    dfs(grid, row + 1, col) //down
-    dfs(grid, row, col - 1) //left
-    dfs(grid, row, col + 1) //right
-}
+  if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] === "0") return;
+  grid[row][col] = "0";
+  dfs(grid, row - 1, col); //up
+  dfs(grid, row + 1, col); //down
+  dfs(grid, row, col - 1); //left
+  dfs(grid, row, col + 1); //right
+};
