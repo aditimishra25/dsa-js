055a65e5ce5ab11f29a7d47a1abf691c07d58e81
Aditi Mishra <aditimishra2501@gmail.com>
2026-07-28 17:14:44 +0530

    kth largest element

--- heap/kth-largest-element-in-an-array.js
+++ heap/kth-largest-element-in-an-array.js
@@ -4,10 +4,13 @@
  * @return {number}
  */
 
+// Insert  → Bubble Up -> starts from end
+// Delete  → Bubble Down -> starts from front
+
 //  Approach-1
 var findKthLargest = function (nums, k) {
-    nums = nums.sort((a, b) => b - a)
-    return nums[k - 1]
+  nums = nums.sort((a, b) => b - a);
+  return nums[k - 1];
 };
 
 //Approach-2
@@ -51,8 +54,7 @@ class MinHeap {
         break;
       }
 
-      [this.heap[parent], this.heap[index]] =
-        [this.heap[index], this.heap[parent]];
+      [this.heap[parent], this.heap[index]] = [this.heap[index], this.heap[parent]];
 
       index = parent;
     }
@@ -66,17 +68,11 @@ class MinHeap {
       let right = index * 2 + 2;
       let smallest = index;
 
-      if (
-        left < this.heap.length &&
-        this.heap[left] < this.heap[smallest]
-      ) {
+      if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
         smallest = left;
       }
 
-      if (
-        right < this.heap.length &&
-        this.heap[right] < this.heap[smallest]
-      ) {
+      if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
         smallest = right;
       }
 
@@ -84,15 +80,91 @@ class MinHeap {
         break;
       }
 
-      [this.heap[index], this.heap[smallest]] =
-        [this.heap[smallest], this.heap[index]];
+      [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
 
       index = smallest;
     }
   }
 }
 
-var findKthLargest = function(nums, k) {
+var findKthLargest = function (nums, k) {
+  const minHeap = new MinHeap();
+
+  for (const num of nums) {
+    minHeap.push(num);
+
+    if (minHeap.size() > k) {
+      minHeap.pop();
+    }
+  }
+
+  return minHeap.peek();
+};
+
+// -------------------------------revision-1--------------------------------
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  push(val) {
+    this.heap.push(val);
+    this.bubbleUp();
+  }
+
+  pop() {
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+
+    return min;
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      let parent = Math.floor((index - 1) / 2);
+
+      if (this.heap[parent] <= this.heap[index]) break;
+
+      [this.heap[parent], this.heap[index]] = [this.heap[index], this.heap[parent]];
+
+      index = parent;
+    }
+  }
+
+  bubbleDown() {
+    let index = 0;
+
+    while (true) {
+      let left = 2 * index + 1;
+      let right = 2 * index + 2;
+
+      let smallest = index;
+
+      if (left < this.heap.length && this.heap[left] < this.heap[smallest]) smallest = left;
+      if (right < this.heap.length && this.heap[right] < this.heap[smallest]) smallest = right;
+      if(smallest == index) break;
+
+      [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+      index = smallest;
+    }
+  }
+}
+
+var findKthLargest = function (nums, k) {
   const minHeap = new MinHeap();
 
   for (const num of nums) {
